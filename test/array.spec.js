259056f76a9f059e2167ada4f19ca368c58deb50
Iain Reid <iainreid382@gmail.com>
2020-05-18 19:58:54 +0000

    chore: Fix formatting of tests

--- test/array.spec.js
+++ test/array.spec.js
@@ -3,139 +3,139 @@
 const S = require('../')
 
 describe('array-concat', () => {
-    it('merges two arrays', () => {
-      expect(S.Array.concat([4, 5, 6], [1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6])
-    })
+  it('merges two arrays', () => {
+    expect(S.Array.concat([4, 5, 6], [1, 2, 3])).toEqual([1, 2, 3, 4, 5, 6])
+  })
+})
+
+describe('array-copyWithin', () => {
+  it('copies an array chunk to a given index', () => {
+    expect(S.Array.copyWithin(0, 1, 3, [1, 2, 3])).toEqual([2, 3, 3])
+  })
+})
+
+describe('array-entries', () => {
+  it('creates an iterator from an array', () => {
+    expect(typeof S.Array.entries([1, 2, 3])[Symbol.iterator]).toEqual('function')
+  })
+})
+
+describe('array-every', () => {
+  it('check that all elements in an array pass a given condition', () => {
+    expect(S.Array.every((elem) => elem > 0, [1, 2, 3])).toEqual(true)
+  })
+})
+
+describe('array-fill', () => {
+  it('fills an array with the provided value', () => {
+    expect(S.Array.fill(1, Array(5))).toStrictEqual([1, 1, 1, 1, 1])
+  })
+})
+
+describe('array-filter', () => {
+  it('removes non-truthy elements from an array', () => {
+    expect(S.Array.filter((elem) => elem, [1, 0, 0, 1, 1])).toEqual([1, 1, 1])
+  })
+})
+
+describe('array-find', () => {
+  it('finds the first element in an array that matches the given condition', () => {
+    expect(S.Array.find((elem) => elem.endsWith('r'), ['foo', 'bar', 'baz'])).toEqual('bar')
+  })
+})
+
+describe('array-findIndex', () => {
+  it('finds the first element in an array that matches the given condition', () => {
+    expect(S.Array.findIndex((elem) => elem.endsWith('z'), ['foo', 'bar', 'baz'])).toEqual(2)
+  })
+})
+
+describe('array-flat', () => {
+  const arr = [
+    [1],
+    [[2]],
+    [[[3]]]
+  ]
+
+  it('flattens an array with a given depth (1)', () => {
+    expect(S.Array.flat(1, arr)).toEqual([1, [2], [[3]]])
   })
 
-  describe('array-copyWithin', () => {
-    it('copies an array chunk to a given index', () => {
-      expect(S.Array.copyWithin(0, 1, 3, [1, 2, 3])).toEqual([2, 3, 3])
-    })
+  it('flattens an array with a given depth (2)', () => {
+    expect(S.Array.flat(2, arr)).toEqual([1, 2, [3]])
   })
 
-  describe('array-entries', () => {
-    it('creates an iterator from an array', () => {
-      expect(typeof S.Array.entries([1, 2, 3])[Symbol.iterator]).toEqual('function')
-    })
+  it('flattens an array with a given depth (3)', () => {
+    expect(S.Array.flat(3, arr)).toEqual([1, 2, 3])
+  })
+})
+
+describe('array-flatMap', () => {
+  it('correctly maps a function over an array and then flattens the result', () => {
+    expect(S.Array.flatMap((elem, i) => [elem, elem + i], [1, 2, 3])).toEqual([1, 1, 2, 3, 3, 5])
+  })
+})
+
+describe('array-forEach', () => {
+  it('executes the provided function for each element in an array', () => {
+    let acc = 0
+    S.Array.forEach((elem) => acc += elem, [1, 2, 3])
+    expect(acc).toEqual(6)
+  })
+})
+
+describe('array-includes', () => {
+  it('returns true if the given value is in an array (1)', () => {
+    expect(S.Array.includes('foo', ['foo', 'bar'])).toEqual(true)
   })
 
-  describe('array-every', () => {
-    it('check that all elements in an array pass a given condition', () => {
-      expect(S.Array.every((elem) => elem > 0, [1, 2, 3])).toEqual(true)
-    })
+  it('returns true if the given value is in an array (2)', () => {
+    expect(S.Array.includes('bar', ['foo', 'bar'])).toEqual(true)
   })
 
-  describe('array-fill', () => {
-    it('fills an array with the provided value', () => {
-      expect(S.Array.fill(1, Array(5))).toStrictEqual([1, 1, 1, 1, 1])
-    })
+  it('returns true if the given value is in an array (3)', () => {
+    expect(S.Array.includes('baz', ['foo', 'bar'])).toEqual(false)
+  })
+})
+
+describe('array-indexOf', () => {
+  it('returns the index of an element if found (1)', () => {
+    expect(S.Array.indexOf('foo', ['foo', 'bar'])).toEqual(0)
   })
 
-  describe('array-filter', () => {
-    it('removes non-truthy elements from an array', () => {
-      expect(S.Array.filter((elem) => elem, [1, 0, 0, 1, 1])).toEqual([1, 1, 1])
-    })
+  it('returns the index of an element if found (2)', () => {
+    expect(S.Array.indexOf('bar', ['foo', 'bar'])).toEqual(1)
   })
 
-  describe('array-find', () => {
-    it('finds the first element in an array that matches the given condition', () => {
-      expect(S.Array.find((elem) => elem.endsWith('r'), ['foo', 'bar', 'baz'])).toEqual('bar')
-    })
+  it('returns -1 if an element is not found', () => {
+    expect(S.Array.indexOf('baz', ['foo', 'bar'])).toEqual(-1)
+  })
+})
+
+describe('array-join', () => {
+  it('joins elements in an array with the provided string', () => {
+    expect(S.Array.join('-', ['foo', 'bar', 'baz'])).toEqual('foo-bar-baz')
+  })
+})
+
+describe('array-map', () => {
+  it('correctly maps a function over an array', () => {
+    expect(S.Array.map((elem, i) => elem + i, [1, 2, 3])).toEqual([1, 3, 5])
   })
 
-  describe('array-findIndex', () => {
-    it('finds the first element in an array that matches the given condition', () => {
-      expect(S.Array.findIndex((elem) => elem.endsWith('z'), ['foo', 'bar', 'baz'])).toEqual(2)
-    })
+  it('correctly maps a function over an array (when curried)', () => {
+    expect(S.Array.map((elem, i) => elem + i)([1, 2, 3])).toEqual([1, 3, 5])
   })
+})
 
-  describe('array-flat', () => {
-    const arr = [
-      [1],
-      [[2]],
-      [[[3]]]
-    ]
-
-    it('flattens an array with a given depth (1)', () => {
-      expect(S.Array.flat(1, arr)).toEqual([1, [2], [[3]]])
-    })
-
-    it('flattens an array with a given depth (2)', () => {
-      expect(S.Array.flat(2, arr)).toEqual([1, 2, [3]])
-    })
-
-    it('flattens an array with a given depth (3)', () => {
-      expect(S.Array.flat(3, arr)).toEqual([1, 2, 3])
-    })
+describe('array-reduce', () => {
+  it('correctly reduces an array', () => {
+    expect(S.Array.reduce((acc, elem) => acc + elem, 0, [1, 2, 3])).toEqual(6)
   })
+})
 
-  describe('array-flatMap', () => {
-    it('correctly maps a function over an array and then flattens the result', () => {
-      expect(S.Array.flatMap((elem, i) => [elem, elem + i], [1, 2, 3])).toEqual([1, 1, 2, 3, 3, 5])
-    })
+describe('array-some', () => {
+  it('check that no elements in an array pass a given condition', () => {
+    expect(S.Array.some((elem) => elem > 2, [1, 2, 3])).toEqual(true)
   })
-
-  describe('array-forEach', () => {
-    it('executes the provided function for each element in an array', () => {
-      let acc = 0
-      S.Array.forEach((elem) => acc += elem, [1, 2, 3])
-      expect(acc).toEqual(6)
-    })
-  })
-
-  describe('array-includes', () => {
-    it('returns true if the given value is in an array (1)', () => {
-      expect(S.Array.includes('foo', ['foo', 'bar'])).toEqual(true)
-    })
-
-    it('returns true if the given value is in an array (2)', () => {
-      expect(S.Array.includes('bar', ['foo', 'bar'])).toEqual(true)
-    })
-
-    it('returns true if the given value is in an array (3)', () => {
-      expect(S.Array.includes('baz', ['foo', 'bar'])).toEqual(false)
-    })
-  })
-
-  describe('array-indexOf', () => {
-    it('returns the index of an element if found (1)', () => {
-      expect(S.Array.indexOf('foo', ['foo', 'bar'])).toEqual(0)
-    })
-
-    it('returns the index of an element if found (2)', () => {
-      expect(S.Array.indexOf('bar', ['foo', 'bar'])).toEqual(1)
-    })
-
-    it('returns -1 if an element is not found', () => {
-      expect(S.Array.indexOf('baz', ['foo', 'bar'])).toEqual(-1)
-    })
-  })
-
-  describe('array-join', () => {
-    it('joins elements in an array with the provided string', () => {
-      expect(S.Array.join('-', ['foo', 'bar', 'baz'])).toEqual('foo-bar-baz')
-    })
-  })
-
-  describe('array-map', () => {
-    it('correctly maps a function over an array', () => {
-      expect(S.Array.map((elem, i) => elem + i, [1, 2, 3])).toEqual([1, 3, 5])
-    })
-
-    it('correctly maps a function over an array (when curried)', () => {
-      expect(S.Array.map((elem, i) => elem + i)([1, 2, 3])).toEqual([1, 3, 5])
-    })
-  })
-
-  describe('array-reduce', () => {
-    it('correctly reduces an array', () => {
-      expect(S.Array.reduce((acc, elem) => acc + elem, 0, [1, 2, 3])).toEqual(6)
-    })
-  })
-
-  describe('array-some', () => {
-    it('check that no elements in an array pass a given condition', () => {
-      expect(S.Array.some((elem) => elem > 2, [1, 2, 3])).toEqual(true)
-    })
-  })
+})
--- test/map.spec.js
+++ test/map.spec.js
@@ -3,146 +3,145 @@
 const S = require('../')
 
 describe('map-clear', () => {
-    it('removes all of the elements from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(map.size).toEqual(2)
-      S.Map.clear(map)
-      expect(map.size).toEqual(0)
-    })
-  })
-  
-  describe('map-delete', () => {
-    it('removes the element from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(map.get('foo')).toEqual('1')
-      expect(map.get('bar')).toEqual('2')
-      S.Map.delete('foo', map)
-      expect(map.get('foo')).toEqual(undefined)
-      expect(map.get('bar')).toEqual('2')
-    })
-    
-    it('returns true if the key exists and removes it from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(map.get('foo')).toEqual('1')
-      expect(map.get('bar')).toEqual('2')
-      expect(S.Map.delete('bar', map)).toEqual(true)
-      expect(map.get('foo')).toEqual('1')
-      expect(map.get('bar')).toEqual(undefined)
-    })
-  
-    it('returns false if the key doesn\'t exist in the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(map.get('foo')).toEqual('1')
-      expect(map.get('bar')).toEqual('2')
-      expect(S.Map.delete('baz', map)).toEqual(false)
-      expect(map.get('foo')).toEqual('1')
-      expect(map.get('bar')).toEqual('2')
-    })
-  })
-  
-  describe('map-entries', () => {
-    it('returns an iterator object built from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(Array.from(S.Map.entries(map))).toStrictEqual([
-        ['foo', '1'],
-        ['bar', '2'],
-      ])
-    })
-  })
-  
-  describe('map-forEach', () => {
-    it('calls the callback function for each key/value in the provided Map', () => {
-      const fn = jest.fn()
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      S.Map.forEach(fn, map)
-      expect(fn).toBeCalledTimes(2)
-      expect(fn).toHaveBeenNthCalledWith(1, '1', 'foo', map)
-      expect(fn).toHaveBeenNthCalledWith(2, '2', 'bar', map)
-    })
-  })
-  
-  describe('map-get', () => {
-    it('gets a value from the provided Map', () => {
-      const map = new Map()
-  
-      expect(S.Map.get('foo', map)).toEqual(undefined)
-      map.set('foo', 'bar')
-      expect(S.Map.get('foo', map)).toEqual('bar')
-    })
-  })
-  
-  describe('map-has', () => {
+  it('removes all of the elements from the provided Map', () => {
     const map = new Map()
+
     map.set('foo', '1')
-  
-    it('returns true if the key exists in the provided Map', () => {
-      expect(S.Map.has('foo', map)).toBe(true)
-    })
-    
-    it('returns false if the key does not exist in the provided Map', () => {
-      expect(S.Map.has('bar', map)).toBe(false)
-    })
+    map.set('bar', '2')
+
+    expect(map.size).toEqual(2)
+    S.Map.clear(map)
+    expect(map.size).toEqual(0)
   })
-  
-  describe('map-keys', () => {
-    it('returns an iterator object containing the keys from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(Array.from(S.Map.keys(map))).toStrictEqual([
-        'foo',
-        'bar',
-      ])
-    })
+})
+
+describe('map-delete', () => {
+  it('removes the element from the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(map.get('foo')).toEqual('1')
+    expect(map.get('bar')).toEqual('2')
+    S.Map.delete('foo', map)
+    expect(map.get('foo')).toEqual(undefined)
+    expect(map.get('bar')).toEqual('2')
   })
-  
-  describe('map-set', () => {
-    it('sets a value in the provided Map', () => {
-      const map = new Map()
-  
-      expect(map.get('foo')).toEqual(undefined)
-      S.Map.set('foo', 'bar', map)
-      expect(map.get('foo')).toEqual('bar')
-    })
+
+  it('returns true if the key exists and removes it from the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(map.get('foo')).toEqual('1')
+    expect(map.get('bar')).toEqual('2')
+    expect(S.Map.delete('bar', map)).toEqual(true)
+    expect(map.get('foo')).toEqual('1')
+    expect(map.get('bar')).toEqual(undefined)
   })
-  
-  describe('map-values', () => {
-    it('returns an iterator object containing the values from the provided Map', () => {
-      const map = new Map()
-  
-      map.set('foo', '1')
-      map.set('bar', '2')
-  
-      expect(Array.from(S.Map.values(map))).toStrictEqual([
-        '1',
-        '2',
-      ])
-    })
+
+  it('returns false if the key doesn\'t exist in the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(map.get('foo')).toEqual('1')
+    expect(map.get('bar')).toEqual('2')
+    expect(S.Map.delete('baz', map)).toEqual(false)
+    expect(map.get('foo')).toEqual('1')
+    expect(map.get('bar')).toEqual('2')
   })
-  
+})
+
+describe('map-entries', () => {
+  it('returns an iterator object built from the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(Array.from(S.Map.entries(map))).toStrictEqual([
+      ['foo', '1'],
+      ['bar', '2'],
+    ])
+  })
+})
+
+describe('map-forEach', () => {
+  it('calls the callback function for each key/value in the provided Map', () => {
+    const fn = jest.fn()
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    S.Map.forEach(fn, map)
+    expect(fn).toBeCalledTimes(2)
+    expect(fn).toHaveBeenNthCalledWith(1, '1', 'foo', map)
+    expect(fn).toHaveBeenNthCalledWith(2, '2', 'bar', map)
+  })
+})
+
+describe('map-get', () => {
+  it('gets a value from the provided Map', () => {
+    const map = new Map()
+
+    expect(S.Map.get('foo', map)).toEqual(undefined)
+    map.set('foo', 'bar')
+    expect(S.Map.get('foo', map)).toEqual('bar')
+  })
+})
+
+describe('map-has', () => {
+  const map = new Map()
+  map.set('foo', '1')
+
+  it('returns true if the key exists in the provided Map', () => {
+    expect(S.Map.has('foo', map)).toBe(true)
+  })
+
+  it('returns false if the key does not exist in the provided Map', () => {
+    expect(S.Map.has('bar', map)).toBe(false)
+  })
+})
+
+describe('map-keys', () => {
+  it('returns an iterator object containing the keys from the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(Array.from(S.Map.keys(map))).toStrictEqual([
+      'foo',
+      'bar',
+    ])
+  })
+})
+
+describe('map-set', () => {
+  it('sets a value in the provided Map', () => {
+    const map = new Map()
+
+    expect(map.get('foo')).toEqual(undefined)
+    S.Map.set('foo', 'bar', map)
+    expect(map.get('foo')).toEqual('bar')
+  })
+})
+
+describe('map-values', () => {
+  it('returns an iterator object containing the values from the provided Map', () => {
+    const map = new Map()
+
+    map.set('foo', '1')
+    map.set('bar', '2')
+
+    expect(Array.from(S.Map.values(map))).toStrictEqual([
+      '1',
+      '2',
+    ])
+  })
+})
