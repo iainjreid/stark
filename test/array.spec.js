f4ad73c6d5cbb93fa1e8afa1d9d7cc25bf1f93fb
Iain Reid <iainreid382@gmail.com>
2020-06-25 14:16:38 +0000

    chore: Complete tests for array methods

--- test/array.spec.js
+++ test/array.spec.js
@@ -148,14 +148,142 @@ describe('array-map', () => {
   })
 })
 
+describe('array-pop', () => {
+  const arr = [1, 2, 3]
+
+  it('removes the last element from an array (1)', () => {
+    expect(S.Array.pop(arr)).toEqual(3)
+    expect(arr).toEqual([1, 2])
+  })
+
+  it('removes the last element from an array (2)', () => {
+    expect(S.Array.pop(arr)).toEqual(2)
+    expect(arr).toEqual([1])
+  })
+})
+
+describe('array-push', () => {
+  const arr = [1, 2, 3]
+
+  it('appends an element to an array (1)', () => {
+    expect(S.Array.push(1, arr)).toEqual(4)
+    expect(arr).toEqual([1, 2, 3, 1])
+  })
+
+  it('appends an element to an array (2)', () => {
+    expect(S.Array.push.arity(3)(2, 3, arr)).toEqual(6)
+    expect(arr).toEqual([1, 2, 3, 1, 2, 3])
+  })
+})
+
 describe('array-reduce', () => {
   it('correctly reduces an array', () => {
     expect(S.Array.reduce((acc, elem) => acc + elem, 0, [1, 2, 3])).toEqual(6)
   })
 })
 
+describe('array-reduceRight', () => {
+  it('correctly reduces an array from right to left', () => {
+    expect(S.Array.reduceRight((acc, elem) => acc + elem.toString(), '', [1, 2, 3])).toEqual('321')
+  })
+})
+
+describe('array-reverse', () => {
+  it('reverses an array', () => {
+    expect(S.Array.reverse([1, 2, 3])).toEqual([3, 2, 1])
+  })
+})
+
+describe('array-shift', () => {
+  const arr = [1, 2, 3]
+
+  it('removes the first element from an array (1)', () => {
+    expect(S.Array.shift(arr)).toEqual(1)
+    expect(arr).toEqual([2, 3])
+  })
+
+  it('removes the first element from an array (2)', () => {
+    expect(S.Array.shift(arr)).toEqual(2)
+    expect(arr).toEqual([3])
+  })
+})
+
+describe('array-slice', () => {
+  it('shallow copies elements from an array from a start index to an end index (1)', () => {
+    expect(S.Array.slice(2, 4, [1, 2, 3, 4, 5, 6])).toEqual([3, 4])
+  })
+
+  it('shallow copies elements from an array from a start index to an end index (2)', () => {
+    expect(S.Array.slice(5, 7, [1, 2, 3, 4, 5, 6])).toEqual([6])
+  })
+})
+
 describe('array-some', () => {
   it('check that no elements in an array pass a given condition', () => {
     expect(S.Array.some((elem) => elem > 2, [1, 2, 3])).toEqual(true)
+  })
+})
+
+describe('array-sort', () => {
+  const arr = [5, 2, 9, 4, 0, 6]
+
+  it('returns an array in the correct order as specified a given function (1)', () => {
+    expect(S.Array.sort((a, b) => a > b ? 1 : -1, arr)).toEqual([0, 2, 4, 5, 6, 9])
+  })
+
+  it('returns an array in the correct order as specified a given function (2)', () => {
+    expect(S.Array.sort((a, b) => a < b ? 1 : -1, arr)).toEqual([9, 6, 5, 4, 2, 0])
+  })
+})
+
+describe('array-splice', () => {
+  const arr = [1, 2, 3, 4, 5, 6]
+
+  it('inserts items in an array and returns and emtpy array (1)', () => {
+    expect(S.Array.splice(2, 0, 7, arr)).toEqual([])
+    expect(arr).toEqual([1, 2, 7, 3, 4, 5, 6])
+  })
+
+  it('inserts items in an array and returns and emtpy array (2)', () => {
+    expect(S.Array.splice.arity(5)(4, 0, 8, 9, arr)).toEqual([])
+    expect(arr).toEqual([1, 2, 7, 3, 8, 9, 4, 5, 6])
+  })
+})
+
+describe('array-toLocaleString', () => {
+  const arr = [1, 'a', new Date('18 May 2017 06:37:25 UTC')]
+
+  it('returns a string created by calling "toLocaleString" on each element in an array (1)', () => {
+    expect(S.Array.toLocaleString('en', { timeZone: 'UTC' }, arr)).toEqual('1,a,5/18/2017, 6:37:25 AM')
+  })
+
+  it('returns a string created by calling "toLocaleString" on each element in an array (2)', () => {
+    expect(S.Array.toLocaleString('en', { timeZone: 'Europe/Paris' }, arr)).toEqual('1,a,5/18/2017, 8:37:25 AM')
+  })
+})
+
+describe('array-toString', () => {
+  it('returns a string created by calling "toString" on each element in an array', () => {
+    expect(S.Array.toString([1, 'a', 2, 'b'])).toEqual('1,a,2,b')
+  })
+})
+
+describe('array-unshift', () => {
+  const arr = [1, 2, 3]
+
+  it('prepends an element to an array (1)', () => {
+    expect(S.Array.unshift(1, arr)).toEqual(4)
+    expect(arr).toEqual([1, 1, 2, 3])
+  })
+
+  it('prepends an element to an array (2)', () => {
+    expect(S.Array.unshift.arity(3)(2, 3, arr)).toEqual(6)
+    expect(arr).toEqual([2, 3, 1, 1, 2, 3])
+  })
+})
+
+describe('array-values', () => {
+  it('returns an iterator object containing the values from an array', () => {
+    expect(Array.from(S.Array.values(['foo', 'bar']))).toEqual(['foo', 'bar'])
   })
 })
